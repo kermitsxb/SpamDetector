952cd64004eca52723e963fdff46df27d9b5bd24
kermit <thomas.stocker92@gmail.com>
2016-04-19 14:37:58 +0200

    comm

--- SpamDetection/gui/urls.py
+++ SpamDetection/gui/urls.py
@@ -3,5 +3,6 @@ from gui import views
 
 urlpatterns = [
     url(r'^$', views.upload, name='upload'),
-    url(r'^upload/$', views.upload, name='upload')
+    url(r'^upload/$', views.upload, name='upload'),
+    url(r'^graph/(?P<cols>\w+)/$', views.graphique, name='graph')
 ] 
